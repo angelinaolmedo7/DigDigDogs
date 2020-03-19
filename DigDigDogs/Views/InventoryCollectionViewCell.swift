5bb5bd213611b097d8229ad7ef905550ff791e86
angelinaolmedo7 <angelinaolmedo7@gmail.com>
2020-03-18 23:27:30 -0700

    Inventory layout

---
+++ DigDigDogs/Views/InventoryCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  InventoryCollectionViewCell.swift
+//  DigDigDogs
+//
+//  Created by Angelina Olmedo on 3/18/20.
+//  Copyright © 2020 Angelina Olmedo. All rights reserved.
+//
+
+import UIKit
+
+class InventoryCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
